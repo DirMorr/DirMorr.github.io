a2f375f128102ff9b8c07e1b2b60b15c42c52906
DirMorr <morrisoneverything@gmail.com>
2025-02-01 01:28:42 -0500

    Hopefully fix links

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// This script will allow for smooth scroll behavior (if you add more sections or links later)
-document.querySelectorAll('a').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
     });
